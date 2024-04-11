27ae2e70a60b59783616fe9dab891f283c5356b4
Luke Cheng <lukeecheng@gmail.com>
2024-04-11 01:10:12 -0400

    added index file containing fat free

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+//This is my controller
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require
+require_once ('vendor/autoload.php');
+
+//Instantiate the F3 Base Class
+$f3 = Base::instance();
+
+//Define a default route
+//https://kcheng.greenriverdev.com/328/hello-fat-free/
+$f3->route('GET /', function(){
+    echo '<h1>Howdy!</h1>';
+
+    //Render a view page
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
